a6424e48bc9c891c600bf5dc64a6a7fbc0b6c846
LiKTOi1029 <testingboy5689@gmail.com>
2025-12-26 21:14:57 +0330

    main.lua adding new classes

--- DodgingGame/main.lua
+++ DodgingGame/main.lua
@@ -1,6 +1,37 @@
+AllInstances = {}
+
+Player = {}
+Player.__index = Player
+
+Objects = {}
+Objects.__index = Objects
+
+Obstacles = {}
+Obstacles.__index = Obstacles
+setmetatable(Obstacles, Objects)
+
+function Player.new()
+	local instance = setmetatable({}, Player)
+	instance.x = 0
+	instance.y = 0
+	return instance
+end
+
+function Obstacles.new(x, y)
+	local instance = setmetatable({}, Obstacles)
+	instance.x = x
+	instance.y = y
+	return instance
+end
+
 function love.load()
+	
+	local MainPlayer = Player.new()
+	AllInstances["MainPlayer"] = MainPlayer
 
 end
+
+
 function love.update(dt)
 
 end
